d7167c49329b74be6260a611e611a06a9ec947b3
Francesco Anzalone <francesco.a@mail.com>
2022-10-25 21:42:30 +0100

    Added temporary team and history arrays

--- src/App.js
+++ src/App.js
@@ -7,6 +7,80 @@ import "primereact/resources/primereact.min.css"; //core css
 import "primeicons/primeicons.css"; //icons
 
 function App() {
+  // fetch team from database
+  const team = [
+    {
+      name: "User1",
+      rolesAvailable: ["gar", "revT1", "revT2", "hun"],
+      isAway: false,
+      volunteer: "",
+    },
+    {
+      name: "User2",
+      rolesAvailable: ["gar", "revT1", "revT2"],
+      isAway: false,
+      volunteer: "",
+    },
+    {
+      name: "User3",
+      rolesAvailable: ["gar", "revT1", "revT2", "hun"],
+      isAway: false,
+      volunteer: "",
+    },
+    {
+      name: "User4",
+      rolesAvailable: ["revT1", "revT2", "hun"],
+      isAway: false,
+      volunteer: "",
+    },
+    {
+      name: "User5",
+      rolesAvailable: ["revT1", "revT2", "rep"],
+      isAway: false,
+      volunteer: "",
+    },
+    {
+      name: "User6",
+      rolesAvailable: ["gar", "revT1", "revT2", "mas"],
+      isAway: false,
+      volunteer: "",
+    },
+    {
+      name: "User7",
+      rolesAvailable: ["gar", "revT1", "revT2", "hun"],
+      isAway: false,
+      volunteer: "",
+    },
+    {
+      name: "User8",
+      rolesAvailable: ["revT1", "revT2", "mas"],
+      isAway: false,
+      volunteer: "",
+    },
+  ];
+
+  // fetch history from database
+  let historyDb = [
+    {
+      weekEnding: "2022-10-16",
+      gardener: "",
+      hunter: "",
+      master: "",
+      revT1: "",
+      revT2: "",
+      reporter: "",
+    },
+    {
+      weekEnding: "2022-10-09",
+      gardener: "",
+      hunter: "",
+      master: "",
+      revT1: "",
+      revT2: "",
+      reporter: "",
+    },
+  ];
+
   return (
     <div className="container min-h-screen min-w-full p-4">
       <Navbar />
