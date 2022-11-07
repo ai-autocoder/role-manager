5a1ddd8ed027b89d9ca15eb7d5b9a52a04562d60
Francesco Anzalone <francesco.a@mail.com>
2022-11-07 22:47:14 +0000

    Added code to build userHistory array

--- src/App.js
+++ src/App.js
@@ -12,83 +12,110 @@ import "primereact/resources/primereact.min.css"; //core css
 import "primeicons/primeicons.css"; //icons
 
 function App() {
-  // fetch team from database
+  // Fetch team from database
   const team = [
     {
-      name: "User1",
+      name: "user1",
       rolesAvailable: ["gar", "revT1", "revT2", "hun"],
       isAway: false,
       volunteer: "",
     },
     {
-      name: "User2",
+      name: "user2",
       rolesAvailable: ["gar", "revT1", "revT2"],
       isAway: false,
       volunteer: "",
     },
     {
-      name: "User3",
+      name: "user3",
       rolesAvailable: ["gar", "revT1", "revT2", "hun"],
       isAway: false,
       volunteer: "",
     },
     {
-      name: "User4",
+      name: "user4",
       rolesAvailable: ["revT1", "revT2", "hun"],
       isAway: false,
       volunteer: "",
     },
     {
-      name: "User5",
+      name: "user5",
       rolesAvailable: ["revT1", "revT2", "rep"],
       isAway: false,
       volunteer: "",
     },
     {
-      name: "User6",
+      name: "user6",
       rolesAvailable: ["gar", "revT1", "revT2", "mas"],
       isAway: false,
       volunteer: "",
     },
     {
-      name: "User7",
+      name: "user7",
       rolesAvailable: ["gar", "revT1", "revT2", "hun"],
       isAway: false,
       volunteer: "",
     },
     {
-      name: "User8",
+      name: "user8",
       rolesAvailable: ["revT1", "revT2", "mas"],
       isAway: false,
       volunteer: "",
     },
   ];
 
-  // fetch history from database
+  // Fetch history from database
   let historyDb = [
     {
       weekEnding: "2022-10-16",
-      gardener: "",
-      hunter: "",
-      master: "",
-      revT1: "",
-      revT2: "",
-      reporter: "",
+      userRoles: [
+        { role: "gar", user: "user5" },
+        { role: "hun", user: "user6" },
+        { role: "mas", user: "user1" },
+        { role: "revT1", user: "user2" },
+        { role: "revT2", user: "user3" },
+        { role: "rep", user: "user4" },
+      ],
     },
     {
-      weekEnding: "2022-10-09",
-      gardener: "",
-      hunter: "",
-      master: "",
-      revT1: "",
-      revT2: "",
-      reporter: "",
+      weekEnding: "2022-10-23",
+      userRoles: [
+        { role: "gar", user: "user6" },
+        { role: "hun", user: "user1" },
+        { role: "mas", user: "user2" },
+        { role: "revT1", user: "user3" },
+        { role: "revT2", user: "user4" },
+        { role: "rep", user: "user5" },
+      ],
+    },
+    {
+      weekEnding: "2022-10-30",
+      userRoles: [
+        { role: "gar", user: "user1" },
+        { role: "hun", user: "user2" },
+        { role: "mas", user: "user3" },
+        { role: "revT1", user: "user4" },
+        { role: "revT2", user: "user5" },
+        { role: "rep", user: "user6" },
+      ],
+    },
+    {
+      weekEnding: "2022-11-06",
+      userRoles: [
+        { role: "gar", user: "user2" },
+        { role: "hun", user: "user3" },
+        { role: "mas", user: "user4" },
+        { role: "revT1", user: "user5" },
+        { role: "revT2", user: "user6" },
+        { role: "rep", user: "user1" },
+      ],
     },
   ];
 
-  // calculate event
+  const historyLength = historyDb.length;
 
-  // pool object (which will be pushed in the history database)
+  // Build pool state object (which will be pushed in the history database)
+  //TODO: filter out disabled roles
   const pool = roles.map((role) => ({
     id: role.id,
     roleCode: role.code,
@@ -96,6 +123,34 @@ function App() {
     volunteer: undefined,
     userSelected: undefined,
   }));
+
+  // Build user history record
+  const userHistory = team.map((user) => ({
+    name: user.name,
+    lastDoneRoles: getUserHistory(user.name),
+  }));
+
+  function getUserHistory(userName) {
+    // Initialize the array with the max value
+    let tempHistoryArray = roles.flatMap((role) =>
+      role.enabled ? { role: role.code, lastDone: historyLength } : []
+    );
+
+    for (const [index, week] of historyDb.entries()) {
+      const key = historyLength - (index + 1); // key represents the week number, last entries are the most recent weeks
+      week.userRoles.forEach((role) => {
+        if (role.user === userName) {
+          // Find role entry in inizialized array
+          const elemIndex = tempHistoryArray.findIndex(
+            (elem) => elem.role === role.role
+          );
+          // if found assign last done to the week number value
+          if (elemIndex !== -1) tempHistoryArray[elemIndex].lastDone = key;
+        }
+      });
+    }
+    return tempHistoryArray;
+  }
 
   function getUsers(roleCode) {
     return team.flatMap((user) =>
